cd588c9c83cf41543bd0fda293e41efbb7c2e5cb
cgswap <baffolobill@yandex.ru>
2011-06-26 05:41:08 +0400

    issue #3

--- media/js/navigation.js
+++ media/js/navigation.js
@@ -11,19 +11,41 @@ function Navigation(opts){
 }
 
 Navigation.prototype = {
+    goto_page: function(page_id){
+	if (page_id[0] != '#') page_id = '#'+page_id;
+
+	var _p = jQuery(page_id);
+	if (_p.length && _p.hasClass('page-base')){
+	    jQuery('.page-base').hide();
+	    _p.show();
+	}
+    },
     arrow_left: function(obj, page_id){
 	this.current_page = jQuery(obj).closest('.'+this.settings.pageBaseClass);
-	this.next_page = jQuery('#'+this.settings.pageIdPrefix+page_id);
+	//this.next_page = jQuery('#'+this.settings.pageIdPrefix+page_id);
+	this.next_page = this.current_page.prev('.'+this.settings.pageBaseClass);
+	if (!this.next_page.length){
+	    this.next_page = jQuery('.'+this.settings.pageBaseClass+':last');
+	}
 
 	this.animate({'direction':'left'});
 
+	Link.setUrlHash(this.next_page.attr('id'));
+	
 	return false;
     },
     arrow_right: function(obj, page_id){
 	this.current_page = jQuery(obj).closest('.'+this.settings.pageBaseClass);
-	this.next_page = jQuery('#'+this.settings.pageIdPrefix+page_id);
+	//this.next_page = jQuery('#'+this.settings.pageIdPrefix+page_id);
+	this.next_page = this.current_page.next('.'+this.settings.pageBaseClass);
+	if (!this.next_page.length){
+	    this.next_page = jQuery('.'+this.settings.pageBaseClass+':first');
+	}
+	
 
 	this.animate({'direction':'right'});
+
+	Link.setUrlHash(this.next_page.attr('id'));
 
 	return false;
     },
